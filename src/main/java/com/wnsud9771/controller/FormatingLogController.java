a9f652d36db5c104bb6c7c9524912d07d4a567fa
jun9771 <jun97718@gmail.com>
2024-11-05 17:31:11 +0900

    recommit

--- src/main/java/com/wnsud9771/controller/FormatingLogController.java
+++ src/main/java/com/wnsud9771/controller/FormatingLogController.java
@@ -1,5 +1,26 @@
 package com.wnsud9771.controller;
 
-public class FormatingLogController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.wnsud9771.dto.receivelog.LogDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/logs")
+@RequiredArgsConstructor
+@Slf4j
+public class FormatingLogController {
+	
+	@Postmapping("/formating")
+    public ResponseEntity<LogDTO> receiveUserFromFirstService(@RequestBody LogDTO logDTO) {
+		log.info("log data {}:", logDTO.getLog_data());
+        return ResponseEntity.ok(receiveLogAndParseTitleService.receiveLogData(logDTO));
+    } 
+	
 }
